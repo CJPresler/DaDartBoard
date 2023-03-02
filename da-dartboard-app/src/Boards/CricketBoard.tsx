360a8d4da9ac8f613385006142c337fd9c2e6671
CoPrez <C.J.Presler@gmail.com>
2023-03-02 00:10:52 -0800

    UI Relayout

--- da-dartboard-app/src/Boards/CricketBoard.tsx
+++ da-dartboard-app/src/Boards/CricketBoard.tsx
@@ -16,6 +16,10 @@ import {
   styled,
   tableCellClasses,
   ButtonGroup,
+  Grid,
+  ListItem,
+  Container,
+  Stack,
 } from "@mui/material";
 import { Icon } from "@mdi/react";
 import {
@@ -32,6 +36,7 @@ import { CricketState } from "../Games/Cricket";
 import { ClientState } from "boardgame.io/dist/types/src/client/client";
 import Confetti from "react-confetti";
 import { Dartboard } from "../Components/Dartboard";
+import { fontSize } from "@mui/system";
 
 const getSectionIcon = (hitCount: number | undefined) => {
   if (hitCount === 0) {
@@ -65,6 +70,14 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:last-child td, &:last-child th": {
     border: 0,
   },
+}));
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
 }));
 
 interface CricketBoardProps {
@@ -110,71 +123,42 @@ export const CricketBoard = (props: CricketBoardProps) => {
   );
 
   return (
-    <main>
-      {(!props.client || !props.client.playerID || !gameState) && (
-        <h3>Loading...</h3>
-      )}
-      {props.client && props.client.playerID && gameState && (
-        <Fragment>
-          {gameState.G.winner && (
-            <Fragment>
-              <Confetti />
-              <h3>{`${props.client.matchData?.at(parseInt(gameState.G.winner))
-                ?.name ?? `Player ${gameState.G.winner}`
-                } Wins!`}</h3>
-            </Fragment>
-          )}
-          {!gameState.G.winner && (
-            <h3>
-              {gameState?.isActive
-                ? "Your Turn"
-                : `${gameState?.ctx.currentPlayer
-                  ? props.client.matchData?.at(
-                    parseInt(gameState.ctx.currentPlayer)
-                  )?.name ?? "Oponnent"
-                  : "Oponnent"
-                }'s Turn`}
-            </h3>
-          )}
+    <Container
+      id="dartboard"
+      maxWidth="md"
+      style={{
+        marginTop: 20,
+        marginBottom: 20,
+      }}
+    >
+      <Stack spacing={2}>
+        {(!props.client || !props.client.playerID || !gameState) && (
+          <h2>Loading...</h2>
+        )}
+        {props.client && props.client.playerID && gameState && (
+          <Fragment>
+            {gameState.G.winner && (
+              <Fragment>
+                <Confetti />
+                <h3>{`${props.client.matchData?.at(parseInt(gameState.G.winner))
+                  ?.name ?? `Player ${gameState.G.winner}`
+                  } Wins!`}</h3>
+              </Fragment>
+            )}
+            {!gameState.G.winner && (
+              <h2>
+                {gameState?.isActive
+                  ? "Your Turn"
+                  : `${gameState?.ctx.currentPlayer
+                    ? props.client.matchData?.at(
+                      parseInt(gameState.ctx.currentPlayer)
+                    )?.name ?? "Oponnent"
+                    : "Oponnent"
+                  }'s Turn`}
+              </h2>
+            )}
 
-          <TableContainer
-            component={Paper}
-            sx={{ maxWidth: 600, margin: "auto" }}
-          >
-            <Table>
-              <TableHead>
-                <StyledTableRow>
-                  <StyledTableCell align="center">Dart 1</StyledTableCell>
-                  <StyledTableCell align="center">Dart 2</StyledTableCell>
-                  <StyledTableCell align="center">Dart 3</StyledTableCell>
-                </StyledTableRow>
-              </TableHead>
-              <TableBody>
-                <StyledTableRow>
-                  {Array.from(Array(3).keys()).map((i) => (
-                    <StyledTableCell component="th" scope="row" align="center">
-                      {gameState?.G.players[gameState?.ctx.currentPlayer]
-                        .dartThrows[0][i]?.ShortName ?? (
-                          <Icon path={mdiArrowProjectile} size="15" />
-                        )}
-                    </StyledTableCell>
-                  ))}
-                </StyledTableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-          <div
-            id="dartboard"
-            style={{
-              marginTop: 20,
-              marginBottom: 20,
-              maxWidth: 500,
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <div style={{ position: "relative" }}>
+            <div style={{ position: 'relative' }}>
               <Dartboard borderGlow={gameState.isActive} highlightSegmentShortName={gameState.G.lastHit?.ShortName} onSegmentClicked={onSegmentHit} />
               <Button
                 className="cta"
@@ -191,119 +175,133 @@ export const CricketBoard = (props: CricketBoardProps) => {
                 gameState.G.turn / gameState.ctx.numPlayers
               )}`}</h3>
             </div>
-          </div>
-          <ButtonGroup variant="outlined" aria-label="outlined button group">
-            {gameState.G.winner && (
-              <Button
-                className="cta"
-                onClick={() => props.client?.moves.rematch?.()}
-              >
-                Rematch
-              </Button>
-            )}
-            {!gameState.G.winner && (
-              <Fragment>
-                <Button
-                  className="cta"
-                  onClick={() => props.client?.events.endTurn?.()}
-                >
-                  End Turn
-                </Button>
-                <Button className="cta" onClick={props.client.undo}>
-                  Undo
-                </Button>
-              </Fragment>
-            )}
-          </ButtonGroup>
-
-          <TableContainer
-            component={Paper}
-            sx={{ maxWidth: 600, margin: "auto" }}
-          >
-            <Table>
-              <TableHead>
-                <StyledTableRow>
-                  <StyledTableCell></StyledTableCell>
-                  <StyledTableCell align="right">15</StyledTableCell>
-                  <StyledTableCell align="right">16</StyledTableCell>
-                  <StyledTableCell align="right">17</StyledTableCell>
-                  <StyledTableCell align="right">18</StyledTableCell>
-                  <StyledTableCell align="right">19</StyledTableCell>
-                  <StyledTableCell align="right">20</StyledTableCell>
-                  <StyledTableCell align="right">Bull</StyledTableCell>
-                  <StyledTableCell align="right">Score</StyledTableCell>
-                </StyledTableRow>
-              </TableHead>
-              <TableBody>
-                {Array.from(Array(gameState?.ctx.numPlayers).keys()).map(
-                  (row) => (
-                    <StyledTableRow>
-                      <StyledTableCell component="th" scope="row">
-                        {!props.client?.matchData?.at(row)?.isConnected && (
-                          <Icon
-                            path={mdiLanDisconnect}
-                            style={{
-                              display: "inline-block",
-                              verticalAlign: "middle",
-                              marginRight: 5,
-                            }}
-                            size={1}
-                            color="#ED3737"
-                          />
-                        )}
-                        {props.client?.matchData?.at(row)?.name
-                          ? props.client?.matchData?.at(row)?.name
-                          : `Player ${row + 1}`}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[15]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[16]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[17]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[18]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[19]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[20]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell align="right">
-                        {getSectionIcon(
-                          gameState?.G.players[row.toString()].sectionsHit[25]
-                        )}
-                      </StyledTableCell>
-                      <StyledTableCell
-                        align="right"
-                        sx={{ fontWeight: "bold" }}
-                      >
-                        {gameState?.G.players[row.toString()].score}
-                      </StyledTableCell>
-                    </StyledTableRow>
-                  )
+            <Grid item container justifyContent={"space-evenly"} style={{ fontWeight: 'bolder', fontSize: 25 }}>
+              {Array.from(Array(3).keys()).map((i) => (
+                <Grid item>
+                  {gameState?.G.players[gameState?.ctx.currentPlayer]
+                    .dartThrows[0][i]?.ShortName ?? (
+                      <Icon path={mdiArrowProjectile} size="40" />
+                    )}
+                </Grid>
+              ))}
+            </Grid>
+            <Container maxWidth="md">
+              <ButtonGroup variant="outlined" aria-label="outlined button group">
+                {gameState.G.winner && (
+                  <Button
+                    className="cta"
+                    onClick={() => props.client?.moves.rematch?.()}
+                  >
+                    Rematch
+                  </Button>
                 )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Fragment>
-      )}
-    </main>
+                {!gameState.G.winner && (
+                  <Fragment>
+                    <Button
+                      className="cta"
+                      onClick={() => props.client?.events.endTurn?.()}
+                    >
+                      End Turn
+                    </Button>
+                    <Button className="cta" onClick={props.client.undo}>
+                      Undo
+                    </Button>
+                  </Fragment>
+                )}
+              </ButtonGroup>
+            </Container>
+
+            <Container maxWidth="md">
+              <TableContainer
+                component={Paper}
+              >
+                <Table>
+                  <TableHead>
+                    <StyledTableRow>
+                      <StyledTableCell></StyledTableCell>
+                      <StyledTableCell align="right">15</StyledTableCell>
+                      <StyledTableCell align="right">16</StyledTableCell>
+                      <StyledTableCell align="right">17</StyledTableCell>
+                      <StyledTableCell align="right">18</StyledTableCell>
+                      <StyledTableCell align="right">19</StyledTableCell>
+                      <StyledTableCell align="right">20</StyledTableCell>
+                      <StyledTableCell align="right">Bull</StyledTableCell>
+                      <StyledTableCell align="right">Score</StyledTableCell>
+                    </StyledTableRow>
+                  </TableHead>
+                  <TableBody>
+                    {Array.from(Array(gameState?.ctx.numPlayers).keys()).map(
+                      (row) => (
+                        <StyledTableRow style={gameState.ctx.currentPlayer === row.toString() ? { boxShadow: "inset 0px 0px 35px -7px #00BAFF" } : undefined}>
+                          <StyledTableCell component="th" scope="row">
+                            {!props.client?.matchData?.at(row)?.isConnected && (
+                              <Icon
+                                path={mdiLanDisconnect}
+                                style={{
+                                  display: "inline-block",
+                                  verticalAlign: "middle",
+                                  marginRight: 5,
+                                }}
+                                size={1}
+                                color="#ED3737"
+                              />
+                            )}
+                            {props.client?.matchData?.at(row)?.name
+                              ? props.client?.matchData?.at(row)?.name
+                              : `Player ${row + 1}`}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[15]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[16]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[17]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[18]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[19]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[20]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell align="right">
+                            {getSectionIcon(
+                              gameState?.G.players[row.toString()].sectionsHit[25]
+                            )}
+                          </StyledTableCell>
+                          <StyledTableCell
+                            align="right"
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            {gameState?.G.players[row.toString()].score}
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      )
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Container>
+          </Fragment>
+        )}
+      </Stack>
+
+    </Container>
   );
 };
